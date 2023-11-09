c4d56906316989b5a87ab2b9054ac38e8e4ef85b
Vanessa Dickey <vcdickey@gmail.com>
2023-11-09 20:47:54 +0000

    imports cleaned up

--- main.py
+++ main.py
@@ -1,30 +1,28 @@
-from fastapi import FastAPI
-from models import Hero, Ability, AbilityType, Relationship, RelationshipType
-from database import engine
-from sqlalchemy import select
-from schemas import HeroModel, AbilityModel, AbilityTypeModel, RelationshipModel, RelationshipTypeModel
+from fastapi import FastAPI, Depends
+import fastapi
+from models import  MenuItem, Cuisine, Category, Base
+from database import SQLALCHEMY_DATABASE_URL, engine, SessionLocal
+from sqlalchemy import select, create_engine
+from sqlalchemy.orm import sessionmaker, Session
 from fastapi.responses import PlainTextResponse
 from starlette.exceptions import HTTPException as StarletteHTTPException
 from fastapi.middleware.cors import CORSMiddleware
-
-from routers import heroes
+from crud import get_menu_items
+# from routers import heroes
 
 app = FastAPI()
-app.include_router(heroes.router)
 
-origins = [
-    "http://8000-coachhallso-fastapidock-5fztzl33j5c.ws-us106.gitpod.io",
-]
+Base.metadata.create_all(bind=engine)
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+@app.get("/api/menu-items")
+def read_menu_items(db: Session = Depends(get_db)):
+    menu_items = get_menu_items(db)
+    return menu_items
 
-@app.exception_handler(StarletteHTTPException)
-async def http_exception_handler(request, exc):
-    print(f"{repr(exc)}")
-    return PlainTextResponse(str(exc.detail), status_code=exc.status_code)
+
